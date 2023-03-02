8c7004b3915fee91db190a577ccf0bea279b3c4b
devopsnhc07 <devopsnhc07@gmail.com>
2023-03-02 10:28:03 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -44,7 +44,7 @@ function Footer() {
               </a>
             </div>
             <p className="copyright">
-              Copyright&nbsp;&copy;&nbsp;Nike Shoes Template
+              Copyright&nbsp;&copy;&nbsp;Nike shoe template
             </p>
             <br />
           </div>
